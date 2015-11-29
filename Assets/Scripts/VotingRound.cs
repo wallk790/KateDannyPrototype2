35307f5a468db120be33372e5b8fac3d11951ed7
Danny <shortieofcheese@gmail.com>
2015-11-29 16:56:59 -0500

    Voting and Stuff

--- Assets/Scripts/VotingRound.cs
+++ Assets/Scripts/VotingRound.cs
@@ -6,29 +6,104 @@ public class VotingRound : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
 {
-
-    if (this.tag == "A")
+    if (this.tag == "VOTEA")
     {
-        if (other.CompareTag("VOTEA"))
+        if (other.CompareTag("A"))
         {
             KeepScore.phase2[0] = "a";
         }
 
-        if (other.CompareTag("VOTEB"))
+        if (other.CompareTag("B"))
         {
-            KeepScore.phase2[0] = "b";
+            KeepScore.phase2[1] = "a";
         }
 
-        if (other.CompareTag("VOTEC"))
+        if (other.CompareTag("C"))
         {
-            KeepScore.phase2[0] = "c";
+            KeepScore.phase2[2] = "a";
         }
 
-        if (other.CompareTag("VOTED"))
+        if (other.CompareTag("D"))
         {
-            KeepScore.phase2[0] = "d";
+            KeepScore.phase2[3] = "a";
         }
 
     }
-}
+
+        if (this.tag == "VOTEB")
+        {
+            if (other.CompareTag("A"))
+            {
+                KeepScore.phase2[0] = "b";
+            }
+
+            if (other.CompareTag("B"))
+            {
+                KeepScore.phase2[1] = "b";
+            }
+
+            if (other.CompareTag("C"))
+            {
+                KeepScore.phase2[2] = "b";
+            }
+
+            if (other.CompareTag("D"))
+            {
+                KeepScore.phase2[3] = "b";
+            }
+
+        }
+
+        if (this.tag == "VOTEC")
+        {
+            if (other.CompareTag("A"))
+            {
+                KeepScore.phase2[0] = "c";
+            }
+
+            if (other.CompareTag("B"))
+            {
+                KeepScore.phase2[1] = "c";
+            }
+
+            if (other.CompareTag("C"))
+            {
+                KeepScore.phase2[2] = "c";
+            }
+
+            if (other.CompareTag("D"))
+            {
+                KeepScore.phase2[3] = "c";
+            }
+
+        }
+
+        if (this.tag == "VOTED")
+        {
+            if (other.CompareTag("A"))
+            {
+                KeepScore.phase2[0] = "d";
+            }
+
+            if (other.CompareTag("B"))
+            {
+                KeepScore.phase2[1] = "d";
+            }
+
+            if (other.CompareTag("C"))
+            {
+                KeepScore.phase2[2] = "d";
+            }
+
+            if (other.CompareTag("D"))
+            {
+                KeepScore.phase2[3] = "d";
+            }
+
+        }
+
+
+
+
+    }
 }

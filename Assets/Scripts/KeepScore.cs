0927a2036d3e67b7bb67cb130441174d0af83a1d
Danny <shortieofcheese@gmail.com>
2015-12-03 11:34:02 -0500

    Updated Vote Scene

--- Assets/Scripts/KeepScore.cs
+++ Assets/Scripts/KeepScore.cs
@@ -4,13 +4,13 @@ using UnityEngine.UI;
 
 public class KeepScore : MonoBehaviour {
 
-	public static string[] phase1 = {"l","l","l","l"};
-	public static string[] phase2 = {"j","j","j","j"};
+	public static string[] phase1 = {"b","b","b","b"};
+	public static string[] phase2 = {"b","b","b","b"};
     public static string truth;
 
-	public static int playerAscore = 500;
-	public static int playerBscore = 200;
-	public static int playerCscore = 100;
+	public static int playerAscore = 0;
+	public static int playerBscore = 0;
+	public static int playerCscore = 0;
 	public static int playerDscore = 0;
 
 	Text playerA;
@@ -28,10 +28,10 @@ public class KeepScore : MonoBehaviour {
 	public GameObject truthSticker;
 
 	
-	public Vector3[] PositionsA;
-	public Vector3[] PositionsB;
-	public Vector3[] PositionsC;
-	public Vector3[] PositionsD;
+	public GameObject[] PositionsA;
+	public GameObject[] PositionsB;
+	public GameObject[] PositionsC;
+	public GameObject[] PositionsD;
 		
 	int storageSpotA = 0;
 	int storageSpotB = 0;
@@ -45,7 +45,7 @@ public class KeepScore : MonoBehaviour {
 		Invoke ("Blamed", 1f);
 		Invoke ("Voted", 3f);
 		Invoke ("Truth", 5f);
-		Invoke ("setMatrix", 10f);
+		//Invoke ("setMatrix", 10f);
 
 		playerA = A.GetComponent<Text>();
 		playerB = B.GetComponent<Text>();
@@ -127,47 +127,47 @@ public class KeepScore : MonoBehaviour {
 
 	void BlameStickers (){
 		for (int i = 0; i < occurrences(phase1[0], phase2); i++) {
-			Instantiate (blameSticker, PositionsA[storageSpotA], Quaternion.identity);
+			Instantiate (blameSticker, PositionsA[storageSpotA].transform.position, Quaternion.identity);
 			storageSpotA++;
 		}
 		for (int i = 0; i < occurrences(phase1[1], phase2); i++) {
-			Instantiate (blameSticker, PositionsB[storageSpotB], Quaternion.identity);
+			Instantiate (blameSticker, PositionsB[storageSpotB].transform.position, Quaternion.identity);
 			storageSpotB++;
 		}
 		for (int i = 0; i < occurrences(phase1[2], phase2); i++) {
-			Instantiate (blameSticker, PositionsC [storageSpotC], Quaternion.identity);
+			Instantiate (blameSticker, PositionsC [storageSpotC].transform.position, Quaternion.identity);
 			storageSpotC++;
 		}
 			for (int i = 0; i < occurrences(phase1[3], phase2); i++) {
-			Instantiate (blameSticker, PositionsD[storageSpotD], Quaternion.identity);
+			Instantiate (blameSticker, PositionsD[storageSpotD].transform.position, Quaternion.identity);
 			storageSpotD++;
 		}
 	}
 
 	void VoteStickers (){
 		for (int i = 0; i < occurrences("a", phase2); i++) {
-			Instantiate (voteSticker, PositionsA[storageSpotA], Quaternion.identity);
+			Instantiate (voteSticker, PositionsA[storageSpotA].transform.position, Quaternion.identity);
 			storageSpotA++;
 		}
 		for (int i = 0; i < occurrences("b", phase2); i++) {
-			Instantiate (voteSticker, PositionsB[storageSpotB], Quaternion.identity);
+			Instantiate (voteSticker, PositionsB[storageSpotB].transform.position, Quaternion.identity);
 			storageSpotB++;
 		}
 		for (int i = 0; i < occurrences("c", phase2); i++) {
-			Instantiate (voteSticker, PositionsC [storageSpotC], Quaternion.identity);
+			Instantiate (voteSticker, PositionsC [storageSpotC].transform.position, Quaternion.identity);
 			storageSpotC++;
 		}
 		for (int i = 0; i < occurrences("d", phase2); i++) {
-			Instantiate (voteSticker, PositionsD[storageSpotD], Quaternion.identity);
+			Instantiate (voteSticker, PositionsD[storageSpotD].transform.position, Quaternion.identity);
 			storageSpotD++;
 		}
 	}
 
 	void TruthStickers () {
-		if (phase2[0] == truth) Instantiate (truthSticker, PositionsA[storageSpotA], Quaternion.identity);
-		if (phase2[1] == truth) Instantiate (truthSticker, PositionsB[storageSpotB], Quaternion.identity);
-		if (phase2[2] == truth) Instantiate (truthSticker, PositionsC[storageSpotC], Quaternion.identity);
-		if (phase2[3] == truth) Instantiate (truthSticker, PositionsD[storageSpotD], Quaternion.identity);
+		if (phase2[0] == truth) Instantiate (truthSticker, PositionsA[storageSpotA].transform.position, Quaternion.identity);
+		if (phase2[1] == truth) Instantiate (truthSticker, PositionsB[storageSpotB].transform.position, Quaternion.identity);
+		if (phase2[2] == truth) Instantiate (truthSticker, PositionsC[storageSpotC].transform.position, Quaternion.identity);
+		if (phase2[3] == truth) Instantiate (truthSticker, PositionsD[storageSpotD].transform.position, Quaternion.identity);
 	}
 
 	void setMatrix() {
